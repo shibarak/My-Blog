4b5d52d2bb68189249fbad0cd42cf700c87b54cd
shibarak <schlaud.jesse@gmail.com>
2021-03-25 12:26:45 +0900

    Add postgres, psycopg2-binary, update requirements.txt

--- main.py
+++ main.py
@@ -12,18 +12,19 @@ import email_validator
 from flask_gravatar import Gravatar
 from smtplib import SMTP
 import config
+import os
 my_email = config.EMAIL
 
 password = config.PASSWORD
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
